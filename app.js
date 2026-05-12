376cba211254a62ad3f17d8f9a384a4863c94e97
MercyChristinaLanka <lmercychristina@gmail.com>
2026-05-12 17:46:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+
+    // ✅ HEALTH CHECK FOR TRAFFIC MANAGER
+    if (req.url === "/health") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        return res.end("OK");
+    }
+
+    // MAIN PAGE
     res.writeHead(200, { "Content-Type": "text/html" });
 
     res.end(`
